cb789b57b873396c7cbed384be559b89fe2a6aa3
Raigorx Hellscream <10247765+raigorx@users.noreply.github.com>
2024-09-02 01:45:44 -0400

    update

--- shared.js
+++ shared.js
@@ -28,5 +28,40 @@ window.sharedModule = {
     }
 
     document.body.appendChild(button)
+  },
+  createShowAppStateButton: function () {
+    const label = document.createElement('label')
+    label.setAttribute('for', 'currentPage')
+    label.textContent = 'Current Page'
+    label.style.position = 'fixed'
+    label.style.bottom = '90px'
+    label.style.left = '10px'
+    label.style.zIndex = '9999'
+    label.style.padding = '10px 0'
+    label.style.color = '#333'
+    label.style.fontWeight = 'bold'
+
+    const input = document.createElement('input')
+    input.setAttribute('type', 'number')
+    input.setAttribute('id', 'currentPage')
+    input.setAttribute('name', 'currentPage')
+    input.setAttribute('min', '1')
+    input.setAttribute('max', appGlobalState.globals.maxPage)
+    input.value = appGlobalState.globals.currentPage
+    input.style.position = 'fixed'
+    input.style.bottom = '80px'
+    input.style.left = '10px'
+    input.style.zIndex = '9999'
+    input.style.padding = '10px'
+    input.style.width = '50px'
+    input.style.backgroundColor = '#f4f4f4'
+    input.style.color = '#333'
+    input.style.border = '1px solid #ccc'
+    input.style.borderRadius = '5px'
+    input.style.cursor = 'pointer'
+
+    // button.onclick = () => console.log(JSON.parse(localStorage.getItem(appGlobalStateName)))
+    document.body.appendChild(label)
+    document.body.appendChild(input)
   }
 }
